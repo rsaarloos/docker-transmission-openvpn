208e00cd8bc84a623de1f9d8be288347ca5cc4dd
Rogier Saarloos <rsaarloos@virtual-affairs.com>
2019-11-05 20:04:21 +0100

    removed run as user

--- sabnzbd/start.sh
+++ sabnzbd/start.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 echo "Starting Sabnzbdplus"
-exec sudo -u ${RUN_AS} /etc/init.d/sabnzbdplus start
+exec sudo /etc/init.d/sabnzbdplus start
--- sabnzbd/stop.sh
+++ sabnzbd/stop.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 . /etc/transmission/userSetup.sh
-exec sudo -u ${RUN_AS} /etc/init.d/sabnzbdplus stop
+exec sudo /etc/init.d/sabnzbdplus stop
